b39fcd948e6aadb651d3a4e4e17461bc6a9473a0
Gayatri Patil <108718904+gav1306@users.noreply.github.com>
2024-08-17 20:01:27 +0530

    initial dummy nodes added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -18,15 +18,88 @@ const onDragOver = (event) => {
 
 let id = 0;
 const getId = () => `dndnode_${id++}`;
-
 const nodeOrigin = [0.5, 0.5];
-
 const nodeTypes = { card: Node };
+const initialNodes = [
+  {
+    id: "dndnode_0",
+    position: {
+      x: 375,
+      y: 270,
+    },
+    data: {
+      name: "Initial",
+      description: "Lorem Ipsum is simply dummy text of the printing.",
+    },
+    type: "card",
+    measured: {
+      width: 250,
+      height: 120,
+    },
+    selected: false,
+  },
+  {
+    id: "dndnode_1",
+    position: {
+      x: 141.92420403662456,
+      y: 528,
+    },
+    data: {
+      name: "Middle",
+      description: "Lorem Ipsum",
+    },
+    type: "card",
+    measured: {
+      width: 250,
+      height: 120,
+    },
+    selected: true,
+    dragging: false,
+  },
+  {
+    id: "dndnode_2",
+    position: {
+      x: 445.75666099825935,
+      y: 802.6200743897089,
+    },
+    data: {
+      name: "End",
+      description: "Lorem Ipsum is simply dummy text of the printing.",
+    },
+    type: "card",
+    measured: {
+      width: 250,
+      height: 120,
+    },
+    selected: false,
+    dragging: false,
+  },
+];
+const initialEdges = [
+  {
+    source: "dndnode_0",
+    sourceHandle: "a",
+    target: "dndnode_1",
+    markerEnd: {
+      type: "arrow",
+    },
+    id: "xy-edge__dndnode_0a-dndnode_1",
+  },
+  {
+    source: "dndnode_1",
+    sourceHandle: "a",
+    target: "dndnode_2",
+    markerEnd: {
+      type: "arrow",
+    },
+    id: "xy-edge__dndnode_1a-dndnode_2",
+  },
+];
 
 const Home = () => {
   const [reactFlowInstance, setReactFlowInstance] = useState();
-  const [nodes, setNodes, onNodesChange] = useNodesState([]);
-  const [edges, setEdges, onEdgesChange] = useEdgesState([]);
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
   const [open, setOpen] = useState(false);
   const [dropPosition, setDropPosition] = useState({ x: 0, y: 0 });
   const onConnect = useCallback(
