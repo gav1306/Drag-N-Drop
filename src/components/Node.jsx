8f919287a86b322dca9867b797157d2a8915b8c3
Gayatri Patil <108718904+gav1306@users.noreply.github.com>
2024-08-17 23:01:03 +0530

    node resizer styles updated

--- src/components/Node.jsx
+++ src/components/Node.jsx
@@ -8,7 +8,7 @@ function Node({ data, selected }) {
 
   return (
     <>
-      <NodeResizer minWidth={250} minHeight={100} isVisible={!!selected} />
+      <NodeResizer handleStyle={{ width: "15px", height: "15px", borderRadius: "50%" }} minWidth={250} minHeight={100} isVisible={!!selected} />
       <Handle style={{ width: "15px", height: "15px" }} type="target" position={Position.Top} />
       <div className="border-2 flex p-4 rounded-md shadow-md flex-col gap-2 h-full items-center min-w-[250px] min-h-[100px] bg-secondary">
         <h2 className="font-semibold ">{name}</h2>
